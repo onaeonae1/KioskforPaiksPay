0863a4bee638bb48896edb48fb919ef759658aa3
Jongwon Baek <41908963+onaeonae1@users.noreply.github.com>
2019-11-18 19:15:36 +0900

    마우스 기능 붙임

--- classes.cpp
+++ classes.cpp
@@ -1,21 +1,11 @@
 #include<iostream>
 #include<string>
+#include<Windows.h>
+#include<stdlib.h>
+#include"box.h"
+#define x first
+#define y second
 using namespace std;
-class box {
-private:
-	int x;
-	int y;
-	int wideth;
-	int height;
-	string name;
-public:
-	void view() {
-
-	}
-	void mouseEvent() {
-
-	}
-};
 class box1 : public box { //메인
 	 
 };
@@ -68,18 +58,65 @@ class bill : public box2 {
 class mileageUse : public box2 {
 
 };
-void mainMouse(int x, int y, string a) {
-	//마우스 들어온 x y 알고
-	//이때 호출해야 할 박스 클래스를 알아서
-	//그걸 호출하는거
+pair<int, int> mouseEvent() { //마우스를 직접 받고, 좌표를 전달하는 함수
+	pair<int, int> ret = make_pair(0,0);
+	HANDLE       hIn, hOut;
+	DWORD        dwNOER;
+	INPUT_RECORD rec;
+	COORD        pos = { 0,1 };
+	hIn = GetStdHandle(STD_INPUT_HANDLE);
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleMode(hIn, ENABLE_PROCESSED_INPUT | ENABLE_MOUSE_INPUT);
+	while (TRUE)
+	{
+		ReadConsoleInput(hIn, &rec, 1, &dwNOER);
+		if (rec.EventType == KEY_EVENT && rec.Event.KeyEvent.bKeyDown == TRUE &&
+			rec.Event.KeyEvent.wVirtualKeyCode == VK_ESCAPE)
+		{
+			break;
+		}
+		if (rec.EventType == MOUSE_EVENT)
+		{
+			if (rec.Event.MouseEvent.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED)
+			{
+				SetConsoleCursorPosition(hOut, pos);
+				//printf("X:%2d, Y:%2d", rec.Event.MouseEvent.dwMousePosition.X, rec.Event.MouseEvent.dwMousePosition.Y);
+				return make_pair( rec.Event.MouseEvent.dwMousePosition.X, rec.Event.MouseEvent.dwMousePosition.Y);
+			}
+		}
+	}
+}
+void mainMouse(pair<int, int> mousepos) { //입력받은 마우스의 좌표를 전달받고 클래스 호출하는 함수
 
 }
-void init() {
+void init() { //초기화
 
+}
+void startView() { //맨처음 화면
+	printf("\n");
+	printf("P R E S S   E N T E R   A N Y   K E Y   T O   S T A R T \n");
+	printf("\n");
+	while (true) {
+		char temp = NULL;
+		cin >> temp;
+		if (temp != NULL) {
+			break;
+		}
+		Sleep(500);
+	}
+	printf("W e l c o m e   T o    P a i k ' s   P a y ");
+	printf("\n");
+	Sleep(1000);
+	return;
 }
 int main() {
-	init();
-	while (true) {
-		mainMouse(); //이벤트 처리
+	bool flag = true;
+	startView();
+	system("cls");
+	init(); //초기화
+	while (flag) {
+		pair<int, int> mousepos = mouseEvent();
+		mainMouse(mousepos);
 	}
+	return 0;
 }
