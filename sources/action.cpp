6269f981f062e5f4b626aac9827941b36cc60180
minseok8162 <57658024+minseok8162@users.noreply.github.com>
2019-11-25 21:31:02 +0900

    Update action.cpp

--- sources/action.cpp
+++ sources/action.cpp
@@ -12,16 +12,12 @@
 
 using namespace std;
 
-void mainMouse(int* state, pair<int, int> mousepos) { //입력받은 마우스의 좌표를 전달받고 클래스 호출하는 함수
-
-}
-
 void action_menuBuy(int* state, pair<int, int> input) {
 	int x = input.first;
 	int y = input.second;
 
 
-	
+
 	if (6 <= y && y <= 8) {
 		if (4 <= x && x <= 15) {
 			//메뉴리스트 1
@@ -133,7 +129,7 @@ void action_mileageControl(int* state, pair<int, int> input) {
 			//마일리지 적립 버튼 처리
 		}
 	}
-	
+
 }
 
 void action_discountControl(int* state, pair <int, int> p1) {
@@ -145,16 +141,24 @@ void action_discountControl(int* state, pair <int, int> p1) {
 		if ((x >= 13) && (x <= 60)) {
 			//couponcontrol로 넘어가는 버튼
 		}
-
 		else if ((x >= 43) && (x <= 60)) {
 			//giftCardControl로 넘어가는 버튼
 		}
-		//이전
-		else if ((y >= 32) && (y <= 49)) {
+		else {
+		//아무 일 없음		
+		}
+	}
+		else if ((y >= 32) && (y <= 49)) { //이전
+			
 			if ((x >= 13) && (x <= 60)) {
 				//manuBuy로 돌아가는 버튼
 			}
+			else {
+				//아무 일 없음
+			}
 		}
+		else {
+		//아무 일 없음
 	}
 }
 
@@ -171,6 +175,12 @@ void action_payMethod(int* state, pair <int, int> p1) {
 		else if ((x >= 102) && (x <= 113)) {
 			//cashInput으로 넘어가는 버튼
 		}
+		else {
+			//아무 일 없음
+		}
+	}
+	else {
+		//아무 일 없음
 	}
 
 }
