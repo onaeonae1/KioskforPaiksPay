e2fec3300972536b257ad3960f1bf867b85df819
rlagkswn00 <56250226+rlagkswn00@users.noreply.github.com>
2019-11-26 17:48:51 +0900

    Update box3.h

--- headers/box3.h
+++ headers/box3.h
@@ -1,1 +1,13 @@
 #pragma once
+#include<iostream>
+#include<string>
+#include"box.h"
+class box3 : public box {
+public:
+	box3() {
+		this->setPos(make_pair(1, 1));
+		this->setHeight();
+		this->setWideth();
+		this->setName("box3");
+	}
+};
