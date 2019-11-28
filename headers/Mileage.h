1cd6425fe018b2ca2874f9ccc01fece73b0a9630
Jongwon Baek <41908963+onaeonae1@users.noreply.github.com>
2019-11-28 18:36:04 +0900

    Update Mileage.h

--- headers/Mileage.h
+++ headers/Mileage.h
@@ -1,1 +1,29 @@
 #pragma once
+#include<vector>
+using namespace std;
+class Mileage {
+private:
+	string key; //사용자 구분 key
+	int value; //값
+public:
+	Mileage() { //기본 생성자
+		key = "X";
+		value = 0;
+	}
+	Mileage(string key,  string value) {
+		this->key = key;
+		this->value = stoi(value);
+	}
+	void setKey(string key) {
+		this->key = key;
+	}
+	void setValue(int value) {
+		this->value = value;
+	}
+	string getKey() {
+		return this->key;
+	}
+	int getValue() {
+		return this->value; 
+	}
+};
