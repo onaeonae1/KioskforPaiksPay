b0e8b624df9e1e363a6c73b32b2266d709bb0afe
Jongwon Baek <41908963+onaeonae1@users.noreply.github.com>
2019-12-04 21:23:28 +0900

    메소드들 변경
    -장바구니의 메뉴 변경은 모두 Cmenu를 매개변수로 한다.
    -가능한 옵션버튼을 2개로 제한하기 위해 shot을 건들 수 있을 경우에는 1샷을 기본값으로 가지도록 했다.

--- headers/Bucket.h
+++ headers/Bucket.h
@@ -9,25 +9,30 @@ private:
 	int cnt;//수량
 	int exp;//옵션에 따른 추가금액
 	vector<int> optionManage{ 4,0 }; //영수증에 출력될 옵션 정보
-	//0 : temperature, 1 : size, 2: shots, 3: creams
 public:
 	Cmenu(Menu menu) {
 		this->menu = menu;
 		this->cnt = 1;
 		this->exp = 0;
+		if ((menu.getoptionSet).at(2) == 1) { //샷을 건드릴 수 있는 메뉴라면
+			optionManage.at(2) = 2; //2샷을 기본으로 하도록
+		}
 	}
-	void optionChange(int index, int value) { //수정할 옵션, 수정될 값
+	void optionChange(int index, int value) {
 		if (index >= 0 && index <= 3) {
 			optionManage.at(index) = value;
 		}
 	}
-	void setExp() { //옵션에 따른 추가금액 계산해줌
+	void setExp() { //옵션에 따른 추가금액 계산
 		if (this->optionManage.at(1) == 1) { //사이즈업
 			this->exp += 1000;
 		}
-		if (this->optionManage.at(2) == 1) { //샷추가
+		if (this->optionManage.at(2) == 3) { //샷추가
 			this->exp += 500;
 		}
+	}
+	vector<int> getoptionManage(){
+		return this->optionManage;
 	}
 	Menu getMenu() {
 		return this->menu;
@@ -50,19 +55,27 @@ private:
 	vector<Cmenu> Menulist;
 
 public:
-	bool compareTo(Menu menu1, Menu menu2) { // 기존 메뉴리스트 중 하나인 menu1과 새로 받은 menu2와의 비교
-		if (menu1.getName() == menu2.getName())
-			if (menu1.getSize() == menu2.getSize())
-				if (menu1.getShots() == menu2.getShots())
-					if (menu1.getCreams() == menu2.getCreams())
-						return 1;
-		return 0;
+	bool compareTo(Cmenu cm1, Cmenu cm2) { // 기존 메뉴리스트 중 하나인 menu1과 새로 받은 menu2와의 비교
+		bool ret = true;
+		Menu m1 = cm1.getMenu();
+		Menu m2 = cm2.getMenu();
+		if (m1.getName() == m2.getName()) {
+			vector<int> op1 = cm1.getoptionManage();
+			vector<int> op2 = cm2.getoptionManage();
+			for (int i = 0; i < 4; i++) { //메뉴 비교
+				ret = ret & (op1.at(i) && op2.at(i));
+			}
+		}
+		else {
+			ret = false;
+		}
+		return ret;
 	}
-	void add(Menu menu) {// 더하기 버튼을 클릭하여 수량 +1
+	void add(Cmenu cm1) {// 더하기 버튼을 클릭하여 수량 +1
 		bool isEqual;
 		int index = 0;
 		for (int i = 0; i < Menulist.size(); i++) {
-			isEqual = compareTo(Menulist[i].getMenu(), menu); //i번째 Menulist에 있는 Cmenu의 menu와 추가하고 싶은 menu의 원소들을 비교
+			isEqual = compareTo(Menulist.at(i), cm1); //i번째 Menulist에 있는 Cmenu의 menu와 추가하고 싶은 menu의 원소들을 비교
 			if (isEqual) {
 				index = i;
 				break;
@@ -72,16 +85,15 @@ public:
 			Menulist[index].setCnt(1);
 		}
 		else {//만약 기존에 menulist에 존재하지 않는다면 menulist에 새로 추가
-			Cmenu newMenu(menu);
-			Menulist.push_back(newMenu);
+			Menulist.push_back(cm1);
 
 		}
 	}
-	void minus(Menu menu) {// 빼기 버튼을 클릭하여 수량 -1
+	void minus(Cmenu cm1) {// 빼기 버튼을 클릭하여 수량 -1
 		bool isEqual;
 		for (int i = 0; i < Menulist.size(); i++) {
 
-			isEqual = compareTo(Menulist[i].getMenu(), menu);
+			isEqual = compareTo(Menulist.at(i), cm1);
 
 			if (isEqual) {
 				//개수가 1개면 메뉴 자체를 삭제
@@ -99,11 +111,11 @@ public:
 			}
 		}
 	}
-	void remove(Menu menu) { // X버튼을 통한 메뉴 삭제
+	void remove(Cmenu cm1) { // X버튼을 통한 메뉴 삭제
 		bool isEqual;
 		for (int i = 0; i < Menulist.size(); i++) {
 
-			isEqual = compareTo(Menulist[i].getMenu(), menu);
+			isEqual = compareTo(Menulist[i], cm1);
 
 			if (isEqual) {
 				for (int j = i; j < Menulist.size(); j++) {
@@ -114,6 +126,9 @@ public:
 				break;
 			}
 		}
+	}
+	vector<Cmenu> getMenulist() {
+		return this->Menulist;
 	}
 	int total() {//총 금액
 		int sum = 0;
