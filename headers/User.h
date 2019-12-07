fb95a54242673435bc50c4731e4860dd397babbe
rlagkswn00 <56250226+rlagkswn00@users.noreply.github.com>
2019-12-07 19:45:19 +0900

    현재까지 사용하고자 한 마일리지가 얼마인지 추가해놓음
    추후에 마일리지 사용했다가 다시 되돌릴때 사용

--- headers/User.h
+++ headers/User.h
@@ -1,3 +1,4 @@
+
 #pragma once
 #include"Model.h"
 #include"Bucket.h"
@@ -13,6 +14,7 @@ private:
 	//마일리지 데이터->처음에는 의미x.
 	//key와 일치하는 마일리지가 있다면 메인에서 연결됨
 	Mileage userMileage;
+	int usedMileage=0;
 public:
 	User() {
 		key = "X";
@@ -29,7 +31,16 @@ public:
 	void setKey(string key) {
 		this->key = key;
 	}
+	Mileage getMileage() {
+		return this->userMileage;
+	}
 	void setMileage(Mileage& linkMileage) {
 		this->userMileage = linkMileage;
+	}
+	void setUsedmileage(int num) {
+		usedMileage += num;
+	}
+	int getUsedmileage() {
+		return this->usedMileage;
 	}
 };
