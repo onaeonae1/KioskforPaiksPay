cbd1adfd6347c160b6f9171548339ad57d4825e2
Jongwon Baek <41908963+onaeonae1@users.noreply.github.com>
2019-12-09 22:24:07 +0900

    Error는 어떤 것도 include 하지 않는다

--- headers/Error.h
+++ headers/Error.h
@@ -1,60 +1,72 @@
 #pragma once
-#include "utils.h"
-#include "views.h"
-
-//입력 함수에 int curError = 0 추가 -> state[3] 사용
-/* curError에서 0 -> 정상(print 불필요)/ 1 -> giftierr/ 2 -> logerr
- 3 -> carderr/casherr  / 4 -> couponerr/gifitierr */
+#include<vector>
+#include<stdlib.h>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<iostream>
+#include<Windows.h>
 class Error {
 	//x = 76 , y = 33,34
-public:	
+public:
+	void gotoXY(int x, int y) { //gotoxy
+		COORD posXY = { x,y };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), posXY);
+	}
+	void clearBOX4() {
+		int x = 76, i = 33;
+		for (i = 33; i < 35; i++) {
+			gotoXY(x, i); printf("                                                ");
+		}
+		return;
+	}
 	void Errorview(int value) {
 		switch (value)
 		{
 		case 0: {
-			clear_box4();
-		//출력 x
+			clearBOX4();
+			//출력 x
 			break;
 		}
 		case 1: {
-			clear_box4();
+			clearBOX4();
 			int x = 76, i = 33;
-			gotoxy(x, i); printf("기 프 티 콘 에 러!"); i++;
-			gotoxy(x, i); printf("다시 입력하세요!"); i++;
+			gotoXY(x, i); printf("기 프 티 콘 에 러!"); i++;
+			gotoXY(x, i); printf("다시 입력하세요!"); i++;
 			Sleep(3000);
 			break;
 		}
 		case 2: {
-			clear_box4();
+			clearBOX4();
 			int x = 76, i = 33;
-			gotoxy(x, i); printf("로 그 인 에 러!"); i++;
-			gotoxy(x, i); printf("다시 입력하세요!"); i++;
+			gotoXY(x, i); printf("로 그 인 에 러!"); i++;
+			gotoXY(x, i); printf("다시 입력하세요!"); i++;
 			Sleep(3000);
 			break;
 		}
 		case 3: {
-			clear_box4();
+			clearBOX4();
 			int x = 76, i = 33;
-			gotoxy(x, i); printf("카 드/현 금 에 러!"); i++;
-			gotoxy(x, i); printf("다시 입력하세요!"); i++;
+			gotoXY(x, i); printf("카 드/현 금 에 러!"); i++;
+			gotoXY(x, i); printf("다시 입력하세요!"); i++;
 			Sleep(3000);
 			break;
 		}
 		case 4: {
-			clear_box4();
+			clearBOX4();
 			int x = 76, i = 33;
-			gotoxy(x, i); printf("쿠 폰/상 품 권 에 러!"); i++;
-			gotoxy(x, i); printf("다시 입력하세요!"); i++;
+			gotoXY(x, i); printf("쿠 폰/상 품 권 에 러!"); i++;
+			gotoXY(x, i); printf("다시 입력하세요!"); i++;
 			Sleep(3000);
 			break;
 		}
 		default: {
-			clear_box4();
+			clearBOX4();
 			//출력 x
 			break;
 		}
 		}
-		
+
 	}
 
 };
