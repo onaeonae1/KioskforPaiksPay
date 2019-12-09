b853f442d7689e167a4753e8b8df83ff5e8a6303
rlagkswn00 <56250226+rlagkswn00@users.noreply.github.com>
2019-12-09 21:13:30 +0900

    Update action.cpp

--- sources/action.cpp
+++ sources/action.cpp
@@ -10,39 +10,10 @@
 #include "utils.h"
 #include "Error.h"
 #include "User.h"
+#include "Everything.h
+#include "Model.h>
 
 using namespace std;
-
-class Everything {
-public:
-	Shop shop;
-	User user;
-
-	cardInput ci;
-	cashInput ci2;
-	discountControl dc;
-	mileageControl mc;
-	menuBuy mb;
-	login lg;
-
-	gifticonInput gi;
-	optionControl oc;
-	couponControl cc;
-	giftcardControl gcc;
-	payMethod pm;
-	bill bl;
-	mileageUse mu;
-	int state[5];
-
-	Error e
-	void setState(int num1, int num2, int num3, int num4, int num5) {
-		state[0] = num1;
-		state[1] = num2;
-		state[2] = num3;
-		state[3] = num4;
-		state[4] = num5;
-	}
-};
 
 string getInput(int print) {//동그라미를 출력해야 하면 시작 x, 아니면 0
 	string result = "";
