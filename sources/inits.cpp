b3497d7e0ede94c9cdbc24deb6e1f6a902207061
rlagkswn00 <56250226+rlagkswn00@users.noreply.github.com>
2019-12-09 19:53:07 +0900

    initusedgifti update

--- sources/inits.cpp
+++ sources/inits.cpp
@@ -97,21 +97,19 @@ void setUsedGifti(Everything E) {
 	}
 	out.close();
 }
-void initCoupon(vector<Coupon>& cData) { //사용가능한 쿠폰 불러오기
-	ifstream in("coupon.txt");
+void initUsedGifti(vector<string>& usedGifti) { //사용가능한 기프티콘 데이터 불러오기
+	ifstream in("UsedGifti.txt");
 	if (in.is_open()) {
 		int num;
 		in >> num;
 		while (num--) {
 			string tempString;
 			in >> tempString; //한 줄 읽어오기
-			vector<string> S = split(tempString, '/'); //잘라오기
-			//쿠폰 생성
-			Coupon* coup= new Coupon(S.at(0), stoi(S.at(1))); //key, value
-			cData.push_back(*coup);
-			delete(coup);
+			
+			usedGifti.push_back(tempString);
 		}
 	}
+	in.close();
 }
 void initgiftiCard(vector<giftiCard>& gcData) {
 	ifstream in("gifticard.txt");
