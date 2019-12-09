1d6ba34589345785124ed62bf0d0e755a57b1b13
minseok8162 <57658024+minseok8162@users.noreply.github.com>
2019-12-09 13:59:06 +0900

    Create Everything.h

---
+++ headers/Everything.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Error.h"
+#include "Model.h"
+#include "header.h"
+
+class cardInput;
+class cashInput;
+class discountControl;
+class mileageControl;
+class menuBuy;
+class login;
+
+class gifticonInput;
+class optionControl;
+class couponControl;
+class giftcardControl;
+class payMethod;
+class bill;
+class mileageUse;
+
+
+class Everything {
+public:
+	Shop shop;
+	User user;
+
+	cardInput* ci;
+	cashInput* ci2;
+	discountControl* dc;
+	mileageControl* mc;
+	menuBuy* mb;
+	login* lg;
+
+	gifticonInput* gi;
+	optionControl* oc;
+	couponControl* cc;
+	giftcardControl* gcc;
+	payMethod* pm;
+	bill* bl;
+	mileageUse* mu;
+	int state[5];
+
+	Error e;
+	void setState(int num1, int num2, int num3, int num4, int num5) {
+		state[0] = num1;
+		state[1] = num2;
+		state[2] = num3;
+		state[3] = num4;
+		state[4] = num5;
+	}
+};
