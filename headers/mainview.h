7d0ad5947bfb97cc79677e5f588cc5811b95885f
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2019-12-10 05:22:00 +0900

    mainmouse에서 인자를 적절히 받아 view들을 호출하는 함수

---
+++ headers/mainview.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "declare.h"
+
+using namespace std;
+
+class Everything;
+
+void viewSwitch(Everything E);
+
+void viewState1(Everything E, int i);
+void viewState2(Everything E, int i);
+void viewState3(Everything E, int i);
+void viewState5(Everything E, int i);
